11241de5e51e0fdc4e36858a9d0942c76d10d917
PayneSun <paynesun@126.com>
2016-07-28 10:34:30 +0800

    Create HelloCSharp.cs

---
+++ LearningCSharp/HelloCSharp.cs
@@ -0,0 +1,14 @@
+// Hello C#.
+
+using System;
+
+namespace HelloCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#!");
+        }
+    }
+}
