37f3b56bbca950e51d029e1048bde6e72dc7e526
PayneSun <sunpxyz@gmail.com>
2019-03-08 12:30:27 +0800

    new file:   leetcode/.cproject 	new file:   leetcode/.project

---
+++ leetcode/src/solution_009.cpp
@@ -0,0 +1,22 @@
+/*
+ * LeetCode
+ *
+ * Question 009: Palindrome Number
+ */
+
+#include <sstream>
+#include <algorithm>
+
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        std::stringstream ss;
+        ss << x;
+
+        std::string str = ss.str();
+        std::reverse(str.begin(), str.end());
+
+        return str == ss.str();
+    }
+};
