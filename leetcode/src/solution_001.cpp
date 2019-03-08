b2c0073cc9ff459463fb6e9bf6bed6e644ccf11b
PayneSun <sunpxyz@gmail.com>
2019-03-08 12:30:27 +0800

    new file:   leetcode/.cproject 	new file:   leetcode/.project

---
+++ leetcode/src/solution_001.cpp
@@ -0,0 +1,28 @@
+/*
+ * LeetCode
+ *
+ * Question 001: Two Sum
+ */
+
+#include <vector>
+
+
+class Solution {
+public:
+	std::vector<int> twoSum(std::vector<int>& nums, int target) {
+		std::vector<int> result;
+		for (std::size_t i = 0; i < nums.size(); ++i) {
+			for (std::size_t j = i + 1; j < nums.size(); ++j) {
+				if (nums[i] + nums[j] == target) {
+					result.push_back(i);
+					result.push_back(j);
+					break;
+				}
+			}
+			if (!result.empty()) {
+				break;
+			}
+		}
+		return result;
+	}
+};
