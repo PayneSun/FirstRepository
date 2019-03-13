1988a53e7802da8e6dad1fd7301e01d09a26a211
PayneSun <sunpxyz@gmail.com>
2019-03-13 14:04:15 +0800

    fix

--- cs/cpl/src/ch_1/power.c
+++ cs/cpl/src/ch_1/power.c
@@ -6,17 +6,24 @@
 
 #include <stdio.h>
 
-int power(int m,, int n);
+int power(int, int);
 
-main()
+/*
+ *
+ */
+int main()
 {
 	int i;
 
-	for (i = 0; i < 10; ++i)
+	for (i = 0; i < 10; ++i) {
 		printf("%d %d %d\n", i, power(2,i), power(-3, i));
+	}
 	return 0;
 }
 
+/*
+ *
+ */
 int power(int base, int n)
 {
 	int i, p;
