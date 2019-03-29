1d21f88d9eda74d635faa6c8db84489d504360f9
PayneSun <sunpxyz@gmail.com>
2019-03-29 22:26:37 +0800

    new

--- cs/c3primer/src/ch03_string_vector_array/array.cpp
+++ cs/c3primer/src/ch03_string_vector_array/array.cpp
@@ -1,6 +1,6 @@
 // ch03_string_vector_array
-// vector.cpp
-// 2019/3/26
+// array.cpp
+// 2019/3/29
 
 
 #include <iostream>
@@ -10,3 +10,18 @@
 std::vector<int> ivec{1, 2, 3};
 std::vector<int> ivec2(10, -1);
 std::vector<std::string> svec(5, "hi");
+
+int main()
+{
+	const char ca[] = {'h', 'e', 'l', 'l', 'o'};
+	const char *cp =ca;
+	while (*cp) {
+		std::cout << *cp << std::endl;
+		++cp;
+	}
+
+	int int_arr[] = {0, 1, 2, 3, 4, 5};
+	std::vector<int> ivec(std::begin(int_arr), std::end(int_arr));
+
+	return 0;
+}
