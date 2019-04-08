2532c9483fd70c01f7ab42e99a135ec8ec6e8d0f
PayneSun <sunpxyz@gmail.com>
2019-04-08 09:49:46 +0800

    new

---
+++ cs/c3primer/src/ch06_function/find_char.cpp
@@ -0,0 +1,19 @@
+
+
+
+#include <iostream>
+#include <string>
+
+std::string::size_type find_char(const std::string &s, char c, std::string::size_type &occurs) {
+	auto ret = s.size();
+	occurs = 0;
+	for (decltype(ret) i = 0; i != s.size(); ++i) {
+		if (s[i] == c) {
+			if (ret == s.size()) {
+				ret = i;
+			}
+			++occurs;
+		}
+	}
+	return ret;
+}
