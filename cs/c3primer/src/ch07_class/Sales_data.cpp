da6a0c53d287050239c9ebe9dd09f354a3ca4833
PayneSun <sunpxyz@gmail.com>
2019-04-11 13:56:39 +0800

    new

--- cs/c3primer/src/ch07_class/Sales_data.cpp
+++ cs/c3primer/src/ch07_class/Sales_data.cpp
@@ -32,15 +32,18 @@ Sales_data& Sales_data::combine(const Sales_data &rhs) {
 
 std::istream &read(std::istream &is, Sales_data &item) {
     double price = 0;
+
     is >> item.bookNo >> item.units_sold >> price;
     item.revenue = price * item.units_sold;
-	return is;
+
+    return is;
 }
 
 
 std::ostream &print(std::ostream &os, const Sales_data &item) {
     os << item.isbn() << " " << item.units_sold << " "
        << item.revenue << " " << item.avg_price();
+
     return os;
 }
 
@@ -48,5 +51,6 @@ std::ostream &print(std::ostream &os, const Sales_data &item) {
 Sales_data add(const Sales_data &lhs, const Sales_data &rhs) {
 	Sales_data sum = lhs;
 	sum.combine(rhs);
+
 	return sum;
 }
--- cs/c3primer/src/ch07_class/solution_07_04.cpp
+++ cs/c3primer/src/ch07_class/solution_07_04.cpp
@@ -6,13 +6,30 @@
 
 class Person {
 	std::string name;
-	std::string adds;
+	std::string addr;
+
+	Person(const std::string &name, const std::string &addr) :
+		name(name), addr(addr) {}
 
 	std::string getName() const {
 		return this->name;
 	}
 
 	std::string getAdds() const {
-		return this->adds;
+		return this->addr;
 	}
 };
+
+
+std::istream &read(std::istream &is, Person &per) {
+    is >> per.name >> per.addr;
+
+    return is;
+}
+
+
+std::ostream &print(std::ostream &os, const Person &per) {
+    os << per.getName() << " " << per.getAdds();
+
+    return os;
+}
