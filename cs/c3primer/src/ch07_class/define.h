5d829834f9eb8b1d1260ea853a2f7375b12299b8
PayneSun <sunpxyz@gmail.com>
2019-04-11 10:34:15 +0800

    new

--- cs/c3primer/src/ch07_class/define.h
+++ cs/c3primer/src/ch07_class/define.h
@@ -11,40 +11,6 @@
 #include <vector>
 
 
-// The definition of class Sales_data
-class Sales_data {
-friend Sales_data add(const Sales_data&, const Sales_data&);
-friend std::istream &read(std::istream&, Sales_data&);
-friend std::ostream &print(std::ostream&, const Sales_data&);
-
-public:
-	Sales_data() = default;
-	explicit Sales_data(const std::string &s) : bookNo(s) {}
-	Sales_data(const std::string &s, unsigned n, double p) :
-		       bookNo(s), units_sold(n), revenue(p * n) {}
-	explicit Sales_data(std::istream &);
-
-    std::string isbn() const { return bookNo; }
-    Sales_data& combine(const Sales_data&);
-
-private:
-    double avg_price() const;
-
-    std::string bookNo;
-    unsigned units_sold = 0;
-    double revenue = 0.0;
-};
-
-// The declaration of class Sales_data's interfaces
-Sales_data add(const Sales_data&, const Sales_data&);
-std::istream &read(std::istream&, Sales_data&);
-std::ostream &print(std::ostream&, const Sales_data&);
-std::ostream &operator<<(std::ostream &os, const Sales_data &item);
-std::istream &operator>>(std::istream &os, Sales_data &item);
-Sales_data operator+(const Sales_data &lhs, const Sales_data & rhs);
-bool operator==(const Sales_data &lhs, const Sales_data &rhs);
-
-
 // The definition of class Screen
 class Screen {
 friend class Window_mgr;
