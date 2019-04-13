40784ac2d2f0feb56491c1c02d26e8866d1580ee
PayneSun <sunpxyz@gmail.com>
2019-04-13 17:59:29 +0800

    new

--- cs/c3primer/src/ch07_class/Screen.h
+++ cs/c3primer/src/ch07_class/Screen.h
@@ -2,12 +2,16 @@
 // Screen.h
 // 2019/4/12
 
+
+#include "Window_mgr.h"
+
 #include <iostream>
 #include <istream>
 #include <string>
 
 
 class Screen {
+	friend class Window_mgr;
 public:
 	typedef std::string::size_type pos;
 	//using pos = std::string::size_type;
@@ -22,6 +26,23 @@ public:
 	inline char get(pos ht, pos wd) const;
 	Screen &move(pos r, pos c);
 	void some_member() const;
+	Screen &set(char);
+	Screen &set(pos, pos, char);
+	Screen &display(std::ostream &os)
+	{
+		this->do_display(os);
+		return *this;
+	}
+	const Screen &display(std::ostream &os) const
+	{
+		this->do_display(os);
+		return *this;
+	}
+private:
+	void do_display(std::ostream &os) const
+	{
+		os << this->contents;
+	}
 private:
 	pos cursor = 0;
 	pos height = 0, width = 0;
@@ -50,4 +71,20 @@ inline char Screen::get(pos r, pos c) const
 void Screen::some_member() const
 {
 	++this->access_ctr;
+}
+
+
+inline Screen &Screen::set(char c)
+{
+	this->contents[this->cursor] = c;
+
+	return *this;
+}
+
+
+inline Screen &Screen::set(pos row, pos col, char ch)
+{
+	this->contents[row * this->width + col] = ch;
+
+	return *this;
 }
