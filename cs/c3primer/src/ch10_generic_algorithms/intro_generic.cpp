717d49d0752a9a449c42310a4c92163f70109f68
sunpeng <sunpeng@muc.edu.cn>
2019-04-25 13:53:38 +0800

    new

--- cs/c3primer/src/ch10_generic_algorithms/intro_generic.cpp
+++ cs/c3primer/src/ch10_generic_algorithms/intro_generic.cpp
@@ -9,12 +9,15 @@
 
 
 int main() {
-
 	int ia[] = { 27, 210, 12, 47, 109, 83 };
 	int val = 83;
 
 	int* ret = std::find(std::begin(ia), std::end(ia), val);
 	std::cout << (ret - ia) << std::endl;
+
+	int sum = std::accumulate(std::begin(ia), std::end(ia), 0);
+	std::cout << "Sum of array ia: " << sum << std::endl;
+
 
 	return 0;
 }
