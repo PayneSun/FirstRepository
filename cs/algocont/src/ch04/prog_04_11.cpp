7a6d0bdbfc32571761fd6f06cb712304a3d1dcd7
PayneSun <sunpxyz@gmail.com>
2019-05-13 11:12:30 +0800

    new

---
+++ cs/algocont/src/ch04/prog_04_11.cpp
@@ -0,0 +1,18 @@
+// prog_04_11.cpp
+// 2019.05.13
+
+#include <cstdio>
+#include <cstring>
+
+#define maxn 100
+
+int left, chance;
+char s[maxn], s2[maxn];
+int win, lose;
+
+void guess(char ch);
+
+int main()
+{
+
+}
