865930267d0425ca7010f8adf63500a2e01d7394
PayneSun <sunpxyz@gmail.com>
2019-05-17 21:53:59 +0800

    new added

--- ml/hello-python-library/numpy/sort_search.py
+++ ml/hello-python-library/numpy/sort_search.py
@@ -14,5 +14,17 @@ c = np.array([('raju', 21),('anil', 25),('ravi', 17)], dtype=b)
 # print(np.sort(c, order='name'))
 
 d = np.floor(100 * np.random.random(9))
-print(d)
-print(np.argsort(d))
+# print(d)
+# print(np.argsort(d))
+
+e = np.floor(50 * np.random.random(9).reshape(3,3))
+# print(e)
+# print(np.argmax(e))
+# print(np.argmin(e))
+# print(np.argmax(e[0,...]))
+# print(np.argmin(e[...,1]))
+# print(np.nonzero(e))
+
+f = np.arange(9).reshape(3,3)
+cond = np.mod(f,2) == 0
+print(np.extract(cond,f))
