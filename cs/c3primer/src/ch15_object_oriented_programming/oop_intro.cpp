9616d7c246c0d98fe7242ba5450f1ec4ad46fa4e
PayneSun <sunpxyz@gmail.com>
2019-06-05 10:01:32 +0800

    new

--- cs/c3primer/src/ch15_object_oriented_programming/oop_intro.cpp
+++ cs/c3primer/src/ch15_object_oriented_programming/oop_intro.cpp
@@ -32,6 +32,9 @@ public:
 	Disc_quote() = default;
 	Disc_quote(const std::string &book, double price, std::size_t qty, double disc):
 		Quote(book, price), quantity(qty), discount(disc) {}
+	std::pair<std::size_t, double> discount_policy() const {
+		return {quantity, discount};
+	}
 	double net_price(std::size_t) const = 0;
 protected:
 	std::size_t quantity = 0;
