90968c8a1ba0c647b622bdfe650696632ced0ed1
snpg <sunpxyz@gmail.com>
2019-06-12 18:40:20 +0800

    new

--- cs/c3primer/src/ch16_templates_and_generic_programming/template_argument_deduction.cpp
+++ cs/c3primer/src/ch16_templates_and_generic_programming/template_argument_deduction.cpp
@@ -4,6 +4,8 @@
 
 
 #include <iostream>
+#include <string>
+#include <utility>
 #include <type_traits>
 
 
@@ -20,4 +22,19 @@ auto fcn2(T beg, T end) -> typename std::remove_reference<decltype(*beg)>::type
 	return *beg;
 }
 
+
+int main()
+{
+	std::string s1("hi"), s2;
+
+	s2 = std::move(std::string("bye"));
+	std::cout << "s1: " << s1 << std::endl;
+	std::cout << "s2: " << s2 << std::endl;
+
+	s2 = std::move(s1);
+	std::cout << "s1: " << s1 << std::endl;
+	std::cout << "s2: " << s2 << std::endl;
+
+	return 0;
+}
 
