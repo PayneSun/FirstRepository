350216e1f0d2ac57c1cc45d584e28c254ebdfda3
paynesun <sunpxyz@126.com>
2019-08-10 19:56:36 +0800

    new added project

---
+++ cs/DesignPattern/01_SimpleFactory/SimpleFactory.cpp
@@ -0,0 +1,60 @@
+// SimpleFactory.cpp
+//
+// 2019/8/10
+
+#include <iostream>
+#include <string>
+
+
+class Product
+{
+public:
+	virtual std::string getProductName() = 0;
+};
+
+
+class ConcreteProductA: public Product
+{
+public:
+	std::string getProductName() {
+		return productName;
+	}
+private:
+	std::string productName{"ConcreteProductA"};
+};
+
+
+class ConcreteProductB: public Product
+{
+public:
+	std::string getProductName() {
+		return productName;
+	}
+private:
+	std::string productName{"ConcreteProductB"};
+};
+
+
+class Factory
+{
+public:
+	Product* createProduct(std::string productName) {
+		if (productName == "ConcreteProductA") {
+			return new ConcreteProductA();
+		} else {
+			return new ConcreteProductB();
+		}
+	}
+};
+
+
+int main()
+{
+	Factory fact;
+	std::string productName("ConcreteProductX");
+
+	auto pcp = fact.createProduct(productName);
+	std::cout << pcp->getProductName() << std::endl;
+
+	return 0;
+}
