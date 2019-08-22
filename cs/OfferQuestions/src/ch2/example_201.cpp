1626b561e262de079d15fab0ee5d481a1b53a5d2
paynesun <sunpxyz@126.com>
2019-08-22 22:03:06 +0800

    add

--- cs/OfferQuestions/src/ch2/example_201.cpp
+++ cs/OfferQuestions/src/ch2/example_201.cpp
@@ -9,7 +9,8 @@ class A
 {
 public:
 	A(int n) { value = n; }
-	A(A other) { value = other.value; }
+//	A(A other) { value = other.value; }
+	A(const A &other) { value = other.value; }
 	void Print() { std::cout << value << std::endl; }
 private:
 	int value;
