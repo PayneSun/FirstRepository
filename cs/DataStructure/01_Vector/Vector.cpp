89ec7119fa7ee3fa7acb4fd70f10721d219b2fe1
paynesun <sunpxyz@126.com>
2019-09-22 21:30:12 +0800

    added

--- cs/DataStructure/01_Vector/Vector.cpp
+++ cs/DataStructure/01_Vector/Vector.cpp
@@ -2,7 +2,7 @@
 // Vector.h
 // 2019/09/17
 
-#include "../01_Vector/Vector.h"
+#include "Vector.h"
 
 #include <iostream>
 #include <algorithm>
@@ -36,4 +36,56 @@ void Vector<T>::insert(Rank r, T const &e) {
 	_size++;
 
 	return r;
+}
+
+
+//
+template<typename T>
+int Vector<T>::remove(Rank lo, Rank hi) {
+	if (lo == hi) { return 0; }
+	while (hi < _size) {
+		_elem[lo++] = _elem[hi++];
+	}
+	_size = lo;
+//	shrink();
+	return hi-lo;
+}
+
+
+//
+template<typename T>
+T Vector<T>::remove(Rank r) {
+	T e = _elem[r];
+	remove(r, r+1);
+	return e;
+}
+
+
+//
+template<typename T>
+Rank Vector<T>::find(T const &e, Rank lo, Rank hi) const {
+	while ((lo < hi--) && e != _elem[hi]) {}
+	return hi;
+}
+
+
+//
+template<typename T>
+int Vector<T>::deduplicate() {
+	int oldSize = _size;
+	Rank i = 1;
+	while (i < _size) {
+		find(_elem[i], 0, i) < 0 ? i++ : remove(i);
+	}
+
+	return oldSize - _size;
+}
+
+
+//
+template<typename T>
+void Vector<T>::traverse(Increase<T> &visit) {
+	for (int i = 0; i <_size; ++i) {
+		visit(_elem[i]);
+	}
 }
--- cs/DataStructure/01_Vector/Vector.h
+++ cs/DataStructure/01_Vector/Vector.h
@@ -5,15 +5,29 @@
 #define DEAFAULT_CAPACITY 10
 typedef int Rank;
 
+
+template<typename T>
+struct Increase {
+	virtual void operator()(T &e) { e++; }
+};
+
+
 template<typename T>
 class Vector
 {
 public:
 	void expand();
 	void insert(Rank r, T const &e);
+	int remove(Rank lo, Rank hi);
+	T remove(Rank r);
+	Rank find(T const &e, Rank lo, Rank hi) const;
+	int deduplicate();
+	void traverse(Increase<T> &visit);
 
 private:
 	int _size;
 	int _capacity;
 	T *_elem;
 };
+
+
