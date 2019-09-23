eb24783c8b9f9d5717312e63899b6d9528e81461
PayneSun <sunpxyz@gmail.com>
2019-09-23 11:55:29 +0800

    new added item

--- cs/OfferQuestions/src/ch2/Q05_PrintListReversely.cpp
+++ cs/OfferQuestions/src/ch2/Q05_PrintListReversely.cpp
@@ -1,55 +1,51 @@
 // Q05_PrintListReversely.cpp
 //
-// 2019.09.20
+// 2019.09.23
 
 #include <iostream>
-#include <cstdio>
+#include <stack>
 #include <cstring>
 
 
-void replaceBlank(char strs[], int length)
+struct ListNode {
+	int value;
+	ListNode *next;
+};
+
+
+void printListReversely(ListNode **head)
 {
-	if (strs == NULL || length <= 0) {
+	if (*head == NULL || head == NULL) {
 		return;
 	}
 
-	int originalLength = 0;
-	int numberOfBlank = 0;
-	int i = 0;
-	while (strs[i] != '\0') {
-		++originalLength;
-		if (strs[i] == ' ') {
-			++numberOfBlank;
-		}
-		++i;
+	std::stack<ListNode *> nodeStack;
+	ListNode *pNode = *head;
+	while (pNode) {
+		nodeStack.push(pNode);
+		pNode = pNode->next;
 	}
 
-	int newLength = originalLength + numberOfBlank * 2;
-	if (newLength > length) {
-		return;
+	while (!nodeStack.empty()) {
+		std::cout << nodeStack.top()->value << std::endl;
+		nodeStack.pop();
 	}
+}
 
-	int indexOfOriginal = originalLength;
-	int indexOfNew = newLength;
-	while (indexOfOriginal >= 0 && indexOfNew > indexOfOriginal) {
-		if (strs[indexOfOriginal] == ' ') {
-			strs[indexOfNew--] = '0';
-			strs[indexOfNew--] = '2';
-			strs[indexOfNew--] = '%';
-		} else {
-			strs[indexOfNew--] = strs[indexOfOriginal];
+
+void printListReverselyRecursively(ListNode *pHead)
+{
+	if (*pHead != NULL) {
+		if (pHead->next != NULL) {
+			printListReverselyRecursively(pHead->next);
 		}
-		indexOfOriginal--;
+
+		std::cout << pHead->value << std::endl;
 	}
 }
 
 
 int main()
 {
-	char strs[50] = "We are happy!";
-
-	replaceBlank(strs, 50);
-	printf("%s", strs);
-
 	return 0;
 }
