e1973ee4961c794779eaded7f94879bdff36ce98
paynesun <sunpxyz@126.com>
2019-10-12 15:42:28 +0800

    added

--- cs/SQL/lesson_10_group_by.sql
+++ cs/SQL/lesson_10_group_by.sql
@@ -8,4 +8,6 @@ select vend_id, count(*) as num_prods from Products group by vend_id;
 
 select cust_id, count(*) as orders from orders group by cust_id having count(*) >= 2;
 
-select cust_id, count(*) as orders from orders where prod_name >= 4 group by cust_id having count(*) >= 2;
+select vend_id, count(*) as num_prods from products where prod_price >= 4 group by vend_id having count(*) >= 2;
+
+select order_num, count(*) as items from orderitems group by order_num having count(*) >= 3 order by items, order_num;
