cb773dc0cd6a089e5ef559ec4ffaf70b81fed32c
PayneSun <sunpxyz@gmail.com>
2020-03-26 22:11:01 +0800

    new added

---
+++ Algo4/src/Fundamentals/Solution_118.java
@@ -0,0 +1,16 @@
+/**
+ * Solution_118.java
+ * 2020.03.26
+ */
+
+package Fundamentals;
+
+public class Solution_118 {
+
+	public static void main(String[] args) {
+		System.out.println('b');
+		System.out.println('b' + 'c');
+		System.out.println((char)('a' + 4));
+	}
+
+}
